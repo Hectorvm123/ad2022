ff853dae2753853dcacf24c23d78ea0387e764c2
Hector Valls <hectorvallsmira2003@gmail.com>
2022-09-15 22:27:24 +0200

    [NEW] Lectura recursiva de ficheros

--- UD01SampleApplication/src/main/java/es/cipfpbatoi/damb/ad/procesador/rutas/ProcesadorRutas.java
+++ UD01SampleApplication/src/main/java/es/cipfpbatoi/damb/ad/procesador/rutas/ProcesadorRutas.java
@@ -1,15 +1,31 @@
 package es.cipfpbatoi.damb.ad.procesador.rutas;
+import java.io.File;
 
 public class ProcesadorRutas {
-	
+
 	public static StringBuilder procesarRuta(String ruta) {
-		
+		File archivoInicial = new File(ruta);
+
 		StringBuilder resultadoProceso = new StringBuilder();
-		
+		revisarCarpeta(archivoInicial.listFiles(), resultadoProceso);
+
 		resultadoProceso.append("Vamos a buscar en la ruta: " + ruta + "\n");
 		resultadoProceso.append("Procesando directorio...\n");
-		
+
 		return resultadoProceso;
 	}
 
+
+	private static void revisarCarpeta (File[] carpeta, StringBuilder resultado){
+		for (File archivo : carpeta) {
+			if (archivo.isFile()) {
+				resultado.append(archivo.getName() + "\n");
+			} else {
+				revisarCarpeta(archivo.listFiles(),resultado);
+
+			}
+		}
+	}
+
 }
+
